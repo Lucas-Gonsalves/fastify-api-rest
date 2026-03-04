dfbf0ebe58d071ca30412cb09f3acb6385249b51
Lucas Gonçalves <lucasluz1710@gmail.com>
2026-03-04 16:48:48 -0300

    feat(routes): create post method   The POST method for the transaction route has been created. Using ZOD, the request validation is performed correctly, and if everything goes well, a new transaction is created in the database using Knex.

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,10 +1,28 @@
 import type { FastifyInstance } from 'fastify'
+import { z } from 'zod'
 import { knex } from '../database'
+import { randomUUID } from 'node:crypto'
 
 export async function transactionRoutes(app: FastifyInstance) {
-  app.get('/hello', async () => {
-    const transaction = await knex('transactions').where('amount', 100)
+  app.post('/', async (request, reply) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
 
-    return transaction
+    const { title, amount, type } = createTransactionBodySchema.parse(
+      request.body
+    )
+
+    await knex('transactions').insert({
+      id: randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    })
+    
+    reply.status(201).send()
+
+    return
   })
 }
