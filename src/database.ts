5096af68752df74a81068fb1c09f56d9163d3a33
Lucas Gonçalves <lucasluz1710@gmail.com>
2026-03-06 17:05:40 -0300

    feat(projetct): deploy config   Setting additional configurations to perform the deployment in the project.

--- src/database.ts
+++ src/database.ts
@@ -3,12 +3,15 @@ import setupKnex from 'knex'
 
 import { env } from './env'
 
+const connection =
+  env.DATABASE_CLIENT === 'sqlite'
+    ? { filename: env.DATABASE_URL }
+    : env.DATABASE_URL
+
 export const config: Knex.Config = {
-  client: 'sqlite3',
+  client: env.DATABASE_CLIENT,
   useNullAsDefault: true,
-  connection: {
-    filename: env.DATABASE_URL,
-  },
+  connection,
   migrations: {
     extension: 'ts',
     directory: 'db/migrations',
